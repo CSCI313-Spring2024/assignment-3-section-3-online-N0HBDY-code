14b7fbc33b31840016332632ea379a860a0a03ae
Christopher Dick <christopher.m.dick@ndsu.edu>
2025-03-12 14:05:06 -0500

    Integrated details page into the application

--- src/app/houseing-location/houseing-location.component.ts
+++ src/app/houseing-location/houseing-location.component.ts
@@ -1,10 +1,10 @@
 import {Component, Input} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { HousingLocation } from '../housinglocation';
-
+import { RouterModule, RouterLink, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-houseing-location',
-  imports: [],
+  imports: [RouterModule, RouterLink, RouterOutlet],
   templateUrl: './houseing-location.component.html',
   styleUrl: './houseing-location.component.css'
 })
