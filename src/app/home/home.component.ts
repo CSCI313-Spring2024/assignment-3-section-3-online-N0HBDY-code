ecaf2c543f9a6d1f0d7a2733159964abde6b1ad8
Christopher Dick <christopher.m.dick@ndsu.edu>
2025-03-12 12:36:25 -0500

    Completed Creating HousingLocationComponent and adding it to the Angular app

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { HouseingLocationComponent } from '../houseing-location/houseing-location.component';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HouseingLocationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ src/app/houseing-location/houseing-location.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-houseing-location',
+  imports: [],
+  templateUrl: './houseing-location.component.html',
+  styleUrl: './houseing-location.component.css'
+})
+export class HouseingLocationComponent {
+
+}
